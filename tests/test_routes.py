331679b4e6090b4c476910c7f25724b1fa828bbe
Alex Klufas <aklufas@wellesley.edu>
2021-03-15 10:36:49 -0400

    Added tests for creating a wishlist, having trouble creating an instance of WishList()

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,6 +13,8 @@ from flask_api import status  # HTTP Status Codes
 from service.models import db
 from service.routes import app, init_db
 
+DATABASE_URI = os.getenv("DATABASE_URI", "postgres://postgres:postgres@localhost:5432/testdb")
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -22,20 +24,26 @@ class TestWishListServer(TestCase):
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
-        pass
+        app.debug = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        WishList.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
-        pass
+        db.session.close()
 
     def setUp(self):
         """ This runs before each test """
-        self.app = app.test_client()
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
+      #  self.app = app.test_client() #not sure the purpose of this 
 
     def tearDown(self):
         """ This runs after each test """
-        pass
+        db.session.remove()
+        db.drop_all()
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
@@ -45,3 +53,13 @@ class TestWishListServer(TestCase):
         """ Test index call """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_create_a_wishlist(self):
+            """ Create a wishlist and assert that it exists """
+            wishlist = WishList(id=1, name="My List", category="Clothes")  #trouble is here
+            self.assertTrue(wishlist != None)
+            self.assertEqual(wishlist.id, 1)
+            self.assertEqual(wishlist.name, "My List")
+            self.assertEqual(wishlist.category, "Clothes")
+            self.assertEqual(wishlist.available, True)
+
