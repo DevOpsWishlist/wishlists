a1f60104d44caf44ff6083a3adc712d81095209e
emorexls <63022505+emorexls@users.noreply.github.com>
2021-03-15 12:14:21 -0400

    added test cases - but still need to run nosetests

--- tests/test_models.py
+++ tests/test_models.py
@@ -38,3 +38,23 @@ class TestWishList(unittest.TestCase):
     def test_XXXX(self):
         """ Test something """
         self.assertTrue(True)
+
+    def test_update_a_wishlist(self):
+        """ Update a Wishlist """
+        wishlist = wishlistFactory()
+        logging.debug(wishlist)
+        wishlist.create()
+        logging.debug(wishlist)
+        self.assertEqual(wishlist.name, "FirstWishlist")
+        # Change it an save it
+        wishlist.name = "FirstWishlist"
+        original_id = wishlist.id
+        wishlist.save()
+        self.assertEqual(wishlist.id, original_id)
+        self.assertEqual(wishlist.name, "FirstWishlist")
+        # Fetch it back and make sure the id hasn't changed
+        # but the data did change
+        wishlists = Wishlist.all()
+        self.assertEqual(len(wishlists), 1)
+        self.assertEqual(wishlists[0].id, 1)
+        self.assertEqual(wishlists[0].name, "FirstWishlist")
